0428ade83bd844044c325dcb597a18bbc4fa3f0a
ander <anderag062@gmail.com>
2024-08-14 21:19:09 -0600

    Añadiendo funcionalidad en apartado de alta de alumno

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-class Alumnos{
-    constructor(nombre, apellidos, edad, materiasInscritas, calificaciones){
-        
+class Alumnos {
+    constructor() {
+        this.alumno = [];
+        this.clase = [];
     }
+
+    botonDarDeAlta(nombre, apellidos, edad) {
+        let nuevoAlumno = {nombre, apellidos, edad};  
+        this.alumno.push(nuevoAlumno);
+        console.log(nuevoAlumno);
+        return nuevoAlumno;
+    }
+}
+
+const gestionAlumnos = new Alumnos();
+
+function botonDarDeAlta() { //la funcion unicamente cumple con guardar los datos del tablero, la clase alumno lo convierte en un objeto
+    const nombre = document.getElementById("nombre").value;
+    const apellidos = document.getElementById("apellidos").value;
+    const edad = document.getElementById("edad").value;
+    gestionAlumnos.botonDarDeAlta(nombre, apellidos, edad);
 }
